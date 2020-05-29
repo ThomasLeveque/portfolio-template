c63fb6af516341225b3bd91f3c05f2992d19cc71
ThomasLeveque <thomasl5895@gmail.com>
2020-05-29 13:38:00 +0200

    add _document

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,20 +1,12 @@
 import React from 'react';
 import { AppProps } from 'next/app';
 import { AnimatePresence } from 'framer-motion';
-import Head from 'next/head';
 
-import Header from '../components/header';
+import Header from '@/components/header';
 
 const MyApp = ({ Component, pageProps, router }: AppProps): JSX.Element => {
   return (
     <>
-      <Head>
-        <link
-          href="https://fonts.googleapis.com/css?family=Montserrat:400,700&display=swap"
-          rel="stylesheet"
-        />
-        <link rel="icon" href="/favicon.ico" />
-      </Head>
       <Header />
       <AnimatePresence exitBeforeEnter>
         <Component {...pageProps} key={router.route} />
