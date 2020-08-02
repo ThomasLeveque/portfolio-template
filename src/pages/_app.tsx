a26f4e9a04de43af61632f6b6b48d80a9e48d157
Thomas Leveque <thomasl5895@gmail.com>
2020-08-02 13:17:39 +0200

    Remove _document file

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import { AppProps } from 'next/app';
+import Head from 'next/head';
 import { AnimatePresence } from 'framer-motion';
 
 import { ThemeProvider } from 'styled-components';
@@ -10,13 +11,22 @@ import { GlobalStyles } from '@/styles/global.styles';
 
 const MyApp = ({ Component, pageProps, router }: AppProps): JSX.Element => {
   return (
-    <ThemeProvider theme={myTheme}>
-      <GlobalStyles />
-      <Header />
-      <AnimatePresence exitBeforeEnter>
-        <Component {...pageProps} key={router.route} />
-      </AnimatePresence>
-    </ThemeProvider>
+    <>
+      <Head>
+        <link
+          href="https://fonts.googleapis.com/css?family=Montserrat:400,700&display=swap"
+          rel="stylesheet"
+        />
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+      <ThemeProvider theme={myTheme}>
+        <GlobalStyles />
+        <Header />
+        <AnimatePresence exitBeforeEnter>
+          <Component {...pageProps} key={router.route} />
+        </AnimatePresence>
+      </ThemeProvider>
+    </>
   );
 };
 
